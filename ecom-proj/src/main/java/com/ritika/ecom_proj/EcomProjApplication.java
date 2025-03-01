062c036252a70ef765b1749311d9fd2b9219c8bb
ritika-vedalytics <ritika.yadav@vedalytics.com>
2025-03-01 16:02:51 +0530

    update

--- ecom-proj/src/main/java/com/ritika/ecom_proj/EcomProjApplication.java
+++ ecom-proj/src/main/java/com/ritika/ecom_proj/EcomProjApplication.java
@@ -3,9 +3,11 @@ package com.ritika.ecom_proj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.ritika.ecom_proj")
+@SpringBootApplication
+@ComponentScan(basePackages = "com.ritika.ecom_proj")
 @EntityScan(basePackages = "com.ritika.ecom_proj.model")
 @EnableJpaRepositories(basePackages = "com.ritika.ecom_proj.repo")
 
